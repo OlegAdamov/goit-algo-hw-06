df0b3e0befd93ea394e1551dc8531e55c801f79b
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2026-09-15 19:12:56 +0300

    Class Record complet. All file complet and verified

--- homework_classes.py
+++ homework_classes.py
@@ -1,13 +1,10 @@
 # Система для управління адресною книгою.
 from collections import UserDict
-from pprint import pprint
-
-from colorama_printer_function import print_error, print_success
 
 
 class Field:
     """
-    Базовий клас для полів запису.
+    Base class for record fields.
     """
     def __init__(self, value):
         self.value = value
@@ -18,14 +15,14 @@ class Field:
 
 class Name(Field):
     """
-    Клас для зберігання імені контакту. Обов'язкове поле.
+    A class for storing the contact name. Mandatory field.
     """
     pass
 
 
 class Phone(Field):
     """
-    Клас для зберігання номера телефону. Має валідацію формату (10 цифр).
+    A class for storing a phone number. It includes format validation (10 digits).
     """
     def __init__(self, value):
         if len(value) == 10 and int(value):
@@ -35,35 +32,34 @@ class Phone(Field):
 
 class Record:
     """
-    Клас для зберігання інформації про контакт, включаючи ім'я та список телефонів.
+    A class for storing contact information, including the name and a list of phones.
     """
     def __init__(self, name: str):
 
         self.name = Name(name)
         self.phones = []
 
-        # print_success(f"Record Add phone, self.name: {self.name}, {type(self.name)}")
 
     def add_phone(self, phone: str) -> None:
         """
-        Метод для додавання телефонів.
+        Method for adding phone.
         """
         is_valid_phone = Phone(phone)
         self.phones.append(is_valid_phone)
 
     def remove_phone(self, phone: str) -> None:
         """
-        Метод для видалення телефонів.
+        Method for removing phone.
         """
-        if not self.phones.is_valid_number(phone):
-            return f"Phone number {phone} is not valid"
+        phone_to_remove = self.find_phone(phone)
+        if phone_to_remove:
+            self.phones.remove(phone_to_remove)
+        else:
+            raise ValueError(f"Phone number {phone} was not found in this record.")
         
-        self.phones.phones.remove(phone)
-        return f"The phone number {phone} has been removed."
-
     def edit_phone(self, old_phone: str, new_phone: str) -> None:
         """
-        Метод для редагування телефонів.
+        Method for editing phone.
         """
         is_valid_phone = Phone(new_phone)
         found_old_phone = self.find_phone(old_phone)
@@ -75,10 +71,9 @@ class Record:
 
     def find_phone(self, phone: str) -> str:
         """
-        Метод поошуку телефону.
+        Phone search method.
         """
         for found_phone in self.phones:
-            print(found_phone)
             if found_phone.value == phone:
                 return found_phone
 
@@ -90,28 +85,28 @@ class Record:
 
 class AddressBook(UserDict):
     """
-    Клас для зберігання та управління записами.
+    A class for storing and managing records.
     """
     def add_record(self, value: Record) -> None:
         """
-        Метод додавання записів.
+        Method for adding record.
         """
         self.data[value.name.value] = value
 
     def find(self, name: str) -> Record:
         """
-        Метод пошуку записів за іменем.
+        Method for searching record by name.
         """
         return self.data.get(name, None)
 
     def delete(self, name: str) -> None:
         """
-        Видалення записів за іменем.
+        Deleting record by name.
         """
         if name in self.data:
             del self.data[name]
-        else:
-            raise KeyError(f"A contact with the name {name} was not found in the address book.")
+        # else:
+        #     raise KeyError(f"A contact with the name {name} was not found in the address book.")
 
     def __str__(self) -> str:
         """
@@ -152,8 +147,6 @@ def main():
 
     # Знаходження та редагування телефону для John
     john = book.find("John")
-    # print(john)
-    # pprint(john)
     john.edit_phone("1234567890", "1112223333")
 
     print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
